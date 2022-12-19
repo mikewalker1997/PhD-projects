578b997d35d251b8b83da1833a7213cb4e62e59b
mikewalker1997 <59792944+mikewalker1997@users.noreply.github.com>
2022-12-19 14:45:02 +0000

    Update record_vids_classes.py

--- 1 - Autonomous Data Collection/record_vids_classes.py
+++ 1 - Autonomous Data Collection/record_vids_classes.py
@@ -4,14 +4,12 @@ import cv2
 import time
 import threading
 
-class SmallVialRecords():
+class RecordVideo():
 
-    # Set camera characteristics (frame rate, length of video, size...) for specific routine
-    
-    def __init__(self, video_filename):
+    def __init__(self, video_filename, capture_duration):
 
         self.open = True
-        self.capture_duration = 5
+        self.capture_duration = capture_duration
         self.device_index, self.fps, self.fourcc, self.frameSize = 0, 27, "MJPG", (640, 480)
         self.video_filename = video_filename
         self.video_cap, self.video_writer = cv2.VideoCapture(self.device_index), cv2.VideoWriter_fourcc(*self.fourcc)
@@ -28,62 +26,7 @@ class SmallVialRecords():
                 self.frame_counts += 1
             else:
                 break
-  
-    def stop(self):
 
-        if self.open:
-
-            self.open = False
-            self.video_out.release()
-            self.video_cap.release()
-            cv2.destroyAllWindows()
-        else:
-            pass
-        
-# start video threading
-
-    def start(self):
-        video_thread = threading.Thread(target=self.record)
-        video_thread.start()
-
-
-
-def start_small_vial_recording(video_filename):
-    global video_thread
-
-    video_thread = SmallVialRecords(video_filename)
-    video_thread.start()
-
-    return video_filename
-
-def stop_small_vial_video(video_filename):
-    global video_thread
-    video_thread.stop()
-
-
-class BigVialRecords():
-
-    def __init__(self, video_filename):
-
-        self.open = True
-        self.capture_duration = 20
-        self.device_index, self.fps, self.fourcc, self.frameSize = 0, 27, "MJPG", (640, 480)
-        self.video_filename = video_filename
-        self.video_cap, self.video_writer = cv2.VideoCapture(self.device_index), cv2.VideoWriter_fourcc(*self.fourcc)
-        self.video_out = cv2.VideoWriter(self.video_filename, self.video_writer, self.fps, self.frameSize)
-        self.frame_counts = 1
-        self.start_time = time.time()
-
-    def record(self):
-
-        while int(time.time() - self.start_time) < self.capture_duration:
-            ret, video_frame = self.video_cap.read()
-            if ret:
-                self.video_out.write(video_frame)
-                self.frame_counts += 1
-            else:
-                break
-       
     def stop(self):
 
         if self.open:
@@ -100,14 +43,21 @@ class BigVialRecords():
         video_thread.start()
 
 
-def start_big_vial_recording(video_filename):
+def start_video_recording(video_filename, vial_size_input, capture_duration):
     global video_thread
+    if vial_size_input == int(8):
 
-    video_thread = BigVialRecords(video_filename)
-    video_thread.start()
+        video_thread = RecordVideo(video_filename, capture_duration=5)
+        video_thread.start()
+
+    elif vial_size_input == int(20):
+
+        video_thread = RecordVideo(video_filename, capture_duration=20)
+        video_thread.start()
 
     return video_filename
 
-def stop_big_vial_video(video_filename):
+def stop_video_recording(video_filename):
     global video_thread
     video_thread.stop()
+
